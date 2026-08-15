447324d9f54c1e5d5fb11b16c41b0d6b7db4addf
MrMame <MrMame@laptop.de>
2026-08-15 09:06:38 +0200

    - Drag Component ist jetzt MinMax Fähig und wird über alle anderen gelegt wenn angeklickt wird

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { MonsterTrackerPage } from './features/monster-tracker/components/monster-tracker-page/monster-tracker-page';
-import { MonsterTrackerExPage } from './monster-tracker-ex-page/monster-tracker-ex-page';
+import { MonsterTrackerExPage } from './features/monster-tracker/components/monster-tracker-ex-page/monster-tracker-ex-page';
 
 export const routes: Routes = [
   { path: 'monstertracker', component: MonsterTrackerPage },
--- src/app/features/monster-tracker/components/monster-tracker-ex-page/monster-tracker-ex-page.ts
+++ src/app/features/monster-tracker/components/monster-tracker-ex-page/monster-tracker-ex-page.ts
@@ -1,27 +1,34 @@
 import { Component } from '@angular/core';
 import { DragDropModule, CdkDragEnd } from '@angular/cdk/drag-drop';
-import { NgStyle } from '@angular/common'; // 1. Importieren
+import { MonsterCard} from '../monster-card/monster-card';
 
 interface MovableItem {
   id: number;
   title: string;
   x: number;
   y: number;
+  isMinimised:boolean;
+  toggleMinimise: () => void;
+  showTop:boolean;
 }
-
 
 @Component({
   selector: 'app-monster-tracker-ex-page',
-  imports: [DragDropModule],
+  imports: [DragDropModule,MonsterCard],
   templateUrl: './monster-tracker-ex-page.html',
   styleUrl: './monster-tracker-ex-page.scss',
 })
 export class MonsterTrackerExPage {
  // Liste der verschiebbaren Boxen mit Startpositionen
   items: MovableItem[] = [
-    { id: 1, title: 'Komponente A', x: 50, y: 50 },
-    { id: 2, title: 'Komponente B', x: 300, y: 150 },
+    { id: 1, title: 'Komponente A', x: 50, y: 50 ,isMinimised: false,toggleMinimise: function() { this.isMinimised = !this.isMinimised; }, showTop: false },
+    { id: 2, title: 'Komponente B', x: 300, y: 150 ,isMinimised: false,toggleMinimise: function() { this.isMinimised = !this.isMinimised; }, showTop: false },
   ];
+
+  showBoxTop(item: MovableItem) {
+    this.items.forEach(i => i.showTop = false); // Setzt alle anderen Boxen auf false
+    item.showTop = true;
+  }
 
   // Diese Funktion wird aufgerufen, wenn der Drag beendet wird
   onDragEnded(event: CdkDragEnd, item: MovableItem) {
